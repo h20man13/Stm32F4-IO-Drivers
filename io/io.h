b1cdaef5906f05811427aaa520f2a78e613b03ea
Jacob Bauer <jacobbauer_2021@depauw.edu>
2020-04-11 20:25:14 -0400

    Added basic io functions to make basic adress io even more basic and I also updated timer_src files

---
+++ io/io.h
@@ -0,0 +1,5 @@
+#ifndef IO_H
+#define IO_H
+void out(uint32_t*, uint32_t, uint32_t, uint32_t);
+uint32_t in(uint32_t*, uint32_t, uint32_t);
+#endif
--- timer/timer_vars.c
+++ timer/timer_vars.c
@@ -1,50 +1,29 @@
-const static uint32_t HSI = 16000000;
-const static uint32_t HSE = 8000000;
-const static uint32_t LSI = 32000;
 
-uint32_t (*PLL_SRC_MUX)() = NULL;
-uint32_t (*System_CLK_MUX)() = NULL;
-
-typedef uint32_t prescaler_val;
-typedef uint32_t speed_val;
-
-//initial clock getter functions
-
-speed_val HSI(){
-  return HSI;
-}
-
-speed_val HSE(){
-  return HSE;
-}
-
-speed_val LSI(){
-  return LSI;
-}
 
 //Headers for sample functions
 
 uint32_t Sample_Clock_Speed(speed_val (*Clock)());
 uint32_t Configure_Clock_Speed(void (*Clock)(uint32_t), uint32_t speed);
-void Change_MUX(uint32_t (*MUX)(), uint32_t(*SRC)());
 uint32_t Sample_Prescaler(prescalar_val (*prescaler)());
+
+
 //Accesor Methods for prescalers
 
 const static uint32_t pll_point = 0x40023804;
 
-prescaler_val PLLN(){
+uint32_t PLLN(){
   return (prescalar_val)((*(pll_point) >> 6) & 0b111111111);
 }
 
-prescalar_val PLLP(){
+uint32_t PLLP(){
   return (prescalar_val)(1 << (((*(pll_point) >> 16) & 0b11) + 1));
 }
 
-prescalar_val PLLM(){
+uint32_t PLLM(){
   return (prescalar_val)(*(pll_point) & 0b111111);
 }
 
-prescalar_val AHB1(){
+uint32_t AHB1(){
   int32_t HPRE = (((*(sys_point) >> 4) & 0b1111) - 0b1000) + 1;
   if(HPRE < 0){
     HPRE = 0;
@@ -55,7 +34,7 @@ prescalar_val AHB1(){
   return (prescalar_val)(1 << HPRE);
 }
 
-prescalar_val APB1(){
+uint32_t APB1(){
   int32_t PPRE1 = ((((*(sys_point) >> 9) & 0b111) - 0b100) + 1);
   if(PPRE1 < 0){
     PPRE1 = 0;
@@ -63,7 +42,7 @@ prescalar_val APB1(){
   return (prescalar_val)(1 << PPRE1);
 }
 
-prescalar_val APB2(){
+uint32_t APB2(){
   int32_t PPRE2 = (((*(sys_point) >> 12) & 0b111) - 0b100) + 1;
   if(PPRE2 < 0){
     PPRE2 = 0;
@@ -79,7 +58,7 @@ uint32_t PLL(){
   uint32_t M = Sample_Prescalar(PLLM);
   uint32_t N = Sample_Prescalar(PLLN);
   uint32_t P = Sample_Prescalar(PLLP);
-  return src / M * N / P
+  return src / M * N / P;
 }
 
 uint32_t SYSCLK(){
@@ -168,62 +147,7 @@ void ADC1(uint32_t speed){
       }while(1);
     }
 }
-void APB1(uint32_t speed){
-    uint32_t* cfg_point = (uint32_t*)0x40033808;
-    uint32_t hl = Sample_Clock_Speed(APB1);
-    if(h1 > speed)
-    {
-      int change = h1 - speed;
-      do{
-	int save_change = change;
-        int value = (((((*(cfg_point) >> 16) & 0b11) - 1);
-	if(value < 0){
-	  break;
-	}
-	*cfg_point &= (~(0b11 << 16));
-	*cfg_point |= value << 16;
-	h1 = Sample_Clock_Speed(ADC1);
-        change = (h1 > speed) ? h1 - speed : speed - h1;
-	if(change >= save_change){
-	  int value = (((((*(cfg_point) >> 16) & 0b11) + 1);
-	  *cfg_point &= (~(0b11 << 16));
-	  *cfg_point |= value << 16;
-	  break;
-	}
-      }while(1);
-    }
-    else if(h1 < speed){
-       int change = speed - h1;
-      do{
-	int save_change = change;
-        int value = (((((*(cfg_point) >> 16) & 0b11) + 1);
-	if(value > 3){
-	  break;
-	}
-	*cfg_point &= (~(0b11 << 16));
-	*cfg_point |= value << 16;
-	h1 = Sample_Clock_Speed(ADC1);
-	change = (speed > h1) ? speed - h1 : h1 - speed;
-	if(change >= save_change){
-	  int value = (((((*(cfg_point) >> 16) & 0b11) - 1);
-	  *cfg_point &= (~(0b11 << 16));
-	  *cfg_point |= value << 16;
-	  break;
-	}
-      }while(1);
-    }
-}
-
-
-//Holder Methods
-
-void Change_MUX(uint32_t (*MUX)(), uint32_t (*SRC)()){
-  MUX = src;
-}
-
-uint32_t Sample_Prescaler(prescalar_val (*prescaler)()){
-  return (uint32_t)(*prescaler)();
-}
+		     
 uint32_t Sample_Clock_Speed(uint32_t (*Clock)()){
   return (*Clock)();
 }

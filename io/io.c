bf7ee75788d4c7b81743f9cac48b18de1504e7e2
Jacob Bauer <jacobbauer_2021@depauw.edu>
2020-04-24 12:50:29 -0400

    added function to necissary header files

--- io/io.c
+++ io/io.c
@@ -1,8 +1,10 @@
 #include "io.h"
 
 void out(uint32_t* addr, uint32_t size, uint32_t shift, uint32_t data){
-  clear(addr, size, shift);
-  *addr |= (data << shift);
+  if(data < 1 << size){
+    clear(addr, size, shift);
+    *addr |= (data << shift);
+  }
 }
 
 void clear(uint32_t* addr, uint32_t size, uint32_t shift){

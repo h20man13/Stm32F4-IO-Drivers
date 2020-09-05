eae67bd4d9baa710bf58bde5e72ffea95de291fe
Jacob Bauer <jacobbauer_2021@depauw.edu>
2020-09-05 14:08:27 -0400

    added code to setup a tmux session for RTOS editing

--- scripts/STM32_Load.sh
+++ scripts/STM32_Load.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-rm -f ./Debugger_Info.txt &&
-> ./Debugger_Info.txt &&
-/opt/openocd/bin/openocd --log_output ./Debugger_Info.txt --file /opt/openocd/scripts/board/st_nucleo_f4.cfg &
-gdb-multiarch $1 -ex 'target extended-remote :3333' &&
-var=$(sudo pidof /opt/openocd/bin/openocd) &&
-kill $var &&
-echo ""
-echo ""
-echo "Open OCD Session Summary"
-echo "========================================================================="
-Storage=$(cat ./Debugger_Info.txt) &&
-echo "$Storage"
-echo "========================================================================="
-echo ""
-rm -f ./Debugger_Info.txt;
+cp ./lib/tmux.conf ./ &&
+sed -i 's:OPENOCD-LOG:/opt/openocd/bin/openocd --file /opt/openocd/scripts/board/st_nucleo_f4.cfg:' ./tmux.conf &&
+sed -i 's/CONNECT-DEBUG/gdb-multiarch $1 -ex "target extended-remote :3333"/' ./tmux.conf &&
+sed -i 's:GOTO-MAIN-DIR:cd ../RTOS:' ./tmux.conf &&
+tmux -f './tmux.conf' &&
+tmux a -d -t 'RTOS-DEV-CONSOLE'
+tmux ls | awk '{print $1}'|sed 's/.$//'| xargs -t -n1 tmux kill-session -t &&
+rm ./tmux.conf
